2e4b70a33bdbadc852afc2803e20c070546d6e57
Qykegi <sda2806@mail.ru>
2026-09-24 10:39:10 +0300

    task 02_04 completed

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,5 +1,11 @@
 def lab_02_1():
     print("Hello, World!")
 
-
+def lab_02_02():
+    name = 'Denis'
+    age = 21
+    height = 180.1
+    is_student = True
+    print(f"Name: {name}\nAge: {age}\nHeight: {height} \nstudent: {is_student}")
+    print(f"Name: {type(name)}\nAge: {type(age)}\nHeight: {type(height)} \nstudent: {type(is_student)}")
 
