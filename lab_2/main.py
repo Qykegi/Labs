5afdcaabef2f023b67878a0f9d3a90644b1322ea
Qykegi <sda2806@mail.ru>
2026-09-24 10:47:53 +0300

    task 02_05 completed

--- lab_2/main.py
+++ lab_2/main.py
@@ -2,10 +2,19 @@ def lab_02_1():
     print("Hello, World!")
 
 def lab_02_02():
-    name = 'Denis'
+    name= 'Denis'
     age = 21
-    height = 180.1
+    height = 180.0
     is_student = True
     print(f"Name: {name}\nAge: {age}\nHeight: {height} \nstudent: {is_student}")
     print(f"Name: {type(name)}\nAge: {type(age)}\nHeight: {type(height)} \nstudent: {type(is_student)}")
+
+def lab_02_03():
+    name = input("Enter name - ")
+    secondName = input("Enter second name - ")
+    age = input("Enter age - ")
+    print(f"Name type: {type(name)}; Second name type: {type(secondName)}; Age type: {type(age)}")
+    age = int(age)
+    height = float(input("Enter height(with '.') - "))
+    print(f"Name and type: {name}, {type(name)}; Second name and type: {secondName}, {type(secondName)}; Age and type: {age}, {type(age)}; Height and type: {height}, {type(height)}")
 
