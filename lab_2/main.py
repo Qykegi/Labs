7a62582f0165e943d8e2ed1ca4be135ff0e09811
Qykegi <sda2806@mail.ru>
2026-09-24 11:03:46 +0300

    task 02_06 completed

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,7 +1,7 @@
-def lab_02_1():
+def lab_02_03():
     print("Hello, World!")
 
-def lab_02_02():
+def lab_02_04():
     name= 'Denis'
     age = 21
     height = 180.0
@@ -9,7 +9,7 @@ def lab_02_02():
     print(f"Name: {name}\nAge: {age}\nHeight: {height} \nstudent: {is_student}")
     print(f"Name: {type(name)}\nAge: {type(age)}\nHeight: {type(height)} \nstudent: {type(is_student)}")
 
-def lab_02_03():
+def lab_02_05():
     name = input("Enter name - ")
     secondName = input("Enter second name - ")
     age = input("Enter age - ")
@@ -18,3 +18,8 @@ def lab_02_03():
     height = float(input("Enter height(with '.') - "))
     print(f"Name and type: {name}, {type(name)}; Second name and type: {secondName}, {type(secondName)}; Age and type: {age}, {type(age)}; Height and type: {height}, {type(height)}")
 
+def lab_02_06():
+    a = int(15)
+    b = int(4)
+    print(f"a + b = {a + b};\na - b = {a - b};\na * b: {a * b};\na / b =  {a / b};\na // b = {a // b};\na % b = {a % b};\na ** b = {a ** b}.")
+    print(f"2 + 3 * 4 = {2 + 3 * 4}, (2 + 3) * 4 = {(2 + 3) * 4}")
